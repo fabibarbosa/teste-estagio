5f7aa5bbf0a9e58591b491c0f22da8795b95b674
Fabiano Barbosa <77905346+fabibarbosa@users.noreply.github.com>
2021-03-07 00:29:37 -0300

    Update index.js

--- RESTFULL-API/index.js
+++ RESTFULL-API/index.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { json } = require('body-parser');
 const app = express();
+const PORT = "5000";
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
-const PORT = "5000";
-
-app.listen(PORT, () => {
-    console.log("Hello");
-})
-app.get('/', (req,res) => {
-    res.send("Hello world");
-})
 
 const knex = require('knex')({
     client: 'mysql',
@@ -18,54 +12,100 @@ const knex = require('knex')({
       host : '127.0.0.1',
       user : 'root',
       password : '',
-      database : 'naverdb'
+      database : 'naversdb'
     }
-  });
+});
 
 
-//Rota responsável por exibir todos os navers
+app.listen(PORT, () => {
+    console.log("The port is working");
+})
+app.get('/', (req,res) => {
+    res.send("Insert the route...");
+})
+
+
 app.get('/navers/index', (req,res,next) => {
     knex('navers').then( (dados) => {
         res.json(dados);
     },next)
 });
 
-//Rota responsável por o naver com id especifico
+//Rota responsável por exibir o naver com id especifico
 app.get('/navers/show/:id', (req,res) => {
     const {id} = req.params;
+    //Verificar se o id passado pelo get existe na tabela, e então seleciona todas informações deste id
+    //Apos selecionar as informações ele pegar as informações da tabela de projetos
     knex('navers').where('id', id).first().then( (dados) => {
         if (dados) {
-            res.json(dados)
+            let jsonNaver = dados;
+            knex('projects').where('navers', id).then( (dados) => {
+                jsonNaver.projects = [];
+                console.log(dados);
+                if (dados === undefined || dados.length === 0) {
+                    res.send(jsonNaver);
+                }else {
+                    jsonNaver.projects.push(dados[0].id, dados[0].name,);
+                    res.send(jsonNaver);
+                }
+
+            })
         }else {
             res.send("id not found");
         }
     })
 });
 
+
+//Crias as variaveis com as informações que foram passadas na requisição e depois insere no banco de dados.
 app.post('/navers/store', (req,res) => {
-    console.log(req.body)
-    res.send("Hello");
+    let {name,birthdate,admission_date,job_role} = req.body
+    res.send(req.body);
+    knex('navers').insert([{id: 0, name: name,
+        birthdate: birthdate,
+        admission_date: admission_date,
+         job_role: job_role}])
+         .then(console.log("Hellou"));
 });
 
 
-app.get('/projects/index', () => {
-    knex('navers').then( (dados) => {
+app.get('/projects/index', (req,res,next) => {
+    knex('projects').then( (dados) => {
         res.json(dados);
     },next)
 });
 
-app.get('/projects/show/:id', () => {
+
+//Rota responsável por exibir os projetos os e seus detalhes
+app.get('/projects/show/:id', (req,res) => {
     const {id} = req.params;
+    //Seleciona o projeto com base na requisicao do get
     knex('projects').where('id', id).first().then( (dados) => {
         if (dados) {
-            res.json(dados)
+            console.log(dados.navers);
+            let jsonProjectsNavers = dados.navers.split(',');
+            jsonProjectsNavers = jsonProjectsNavers.map(Number);
+            console.log(jsonProjectsNavers);
+            let jsonProject = dados;
+            console.log(jsonProjectsNavers);
+            // Depos seleciona todos os navers que trabalharam no determinado projeto
+            knex("navers").whereIn('id',jsonProjectsNavers).then( (naversDados) => {
+                console.log(naversDados)
+                jsonProject.navers = naversDados
+        
+                res.send(jsonProject)
+            }) 
+
         }else {
             res.send("id not found");
         }
+    })
+    
 });
 
-app.post('/projects/store', () => {
-    console.log(req.body)
-    res.send("Hello");
+app.post('/projects/store', (req,res) => {
+    let {id,navers} = req.body
+    knex('projects').insert([{id: 0, navers : navers}]).then(console.log("Hellou"));
+    res.send(req.body)
 });
 
